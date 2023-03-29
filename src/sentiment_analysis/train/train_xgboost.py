04d503d7a8235069cabd74692048f836e755b8e7
Manman Zhang <zhoujiuliang0521@gmail.com>
2023-03-29 15:59:57 +0800

    add train_xgboost(init)

---
+++ src/sentiment_analysis/train/train_xgboost.py
@@ -0,0 +1,33 @@
+from src.sentiment_analysis.dataprep.transformations import tf_idf,get_mean_vector,word2vec
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+import xgboost
+
+def train_xgboost(df_clean, tf_idf = True):
+    # choose one of feature engineering methods: tf_idf or word2vec
+    if tf_idf == True:
+        X,y = tf_idf(df_clean)
+    else:
+        X,y = word2vec(df_clean)
+    # split train-test data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, stratify=y, random_state=4263
+    )
+    # Hyperparameters for optimization
+    params = {
+        "learning_rate": [0.001, 0.01, 0.1, 1],
+        "max_depth": [3, 4, 5, 6, 8, 10, 12, 15],
+        "min_child_weight": [1, 3, 5, 7],
+        "gamma": [0.0, 0.1, 0.2, 0.3, 0.4],
+        "colsample_bytree": [0.3, 0, 4, 0.5, 0.7]
+
+    }
+    classifier = xgboost.XGBClassifier()
+    random_search = RandomizedSearchCV(classifier, param_distributions=params, n_iter=10, scoring="accuracy", verbose=3)
+    random_search.fit(X, y)
+    return random_search.best_estimator_
+
+
+
