1a3340e73f9001657bdcfe52bbd272a8f6915387
kelliecsw <kellie.chin@gmail.com>
2023-04-01 17:07:55 +0800

    edited trian_lda

--- src/topic_modelling/train/train_lda.py
+++ src/topic_modelling/train/train_lda.py
@@ -4,45 +4,42 @@ import numpy as np
 import gensim
 from gensim import corpora, models
 
-def train_lda(docs, num_topics=10):
+def train_lda(df, num_topics=20, fe='bow'):
     """
-    Input: list of tokenized documents, number of topics
-    Output: list of 2 lda models (each trained with BoW or tfidf)
+    Input: df with 'stem_clean_text' column, number of topics, feature engineering method 'tfidf'/'bow'
+    Output: list of predicted topics, lda model
     """
-    print('Started training LDA models...')
     # create features
-    print('Creating features...')
+    docs = list(df['stem_clean_text'].apply(lambda x: x.split()))
     # create dictionary
     dictionary = corpora.Dictionary(docs)
     # convert docs into BoW format
     corpus_bow = [dictionary.doc2bow(doc) for doc in docs]
+    corpus = corpus_bow
 
-    # create tfidf model
-    tfidf = models.TfidfModel(corpus_bow)
-    # convert docs into tfidf format
-    corpus_tfidf = tfidf[corpus_bow]
-
-    print('Training LDA model with BoW...')
-    # build LDA model
-    bow_lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus_bow, 
-                                                    id2word=dictionary, 
-                                                    num_topics=num_topics, 
-                                                    random_state=4263, 
-                                                    update_every=1, 
-                                                    chunksize=100, 
-                                                    passes=50, 
-                                                    alpha='auto',
-                                                    per_word_topics=True)
-    print('Training LDA model with tfidf...')
-    tfidf_lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus_tfidf, 
-                                                      id2word=dictionary, 
-                                                      num_topics=num_topics, 
-                                                      random_state=4263, 
-                                                      update_every=1, 
-                                                      chunksize=100, 
-                                                      passes=50, 
-                                                      alpha='auto', 
-                                                      per_word_topics=True)
+    if fe=='tfidf':
+        # create tfidf model
+        tfidf = models.TfidfModel(corpus_bow)
+        # convert docs into tfidf format
+        corpus_tfidf = tfidf[corpus_bow]
+        corpus = corpus_tfidf
     
-    print('Completed training LDA models!')
-    return [bow_lda_model, tfidf_lda_model]
+    # build LDA model
+    lda_model = gensim.models.ldamodel.LdaModel(corpus=corpus, 
+                                                id2word=dictionary, 
+                                                num_topics=num_topics, 
+                                                random_state=4263, 
+                                                update_every=1, 
+                                                chunksize=100, 
+                                                passes=50, 
+                                                alpha='auto',
+                                                per_word_topics=True)
+
+    pred = []
+    for i, row in enumerate(lda_model[corpus]):
+        row = row[0]
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        topic = row[0][0]
+        pred.append(topic)
+    
+    return pred, lda_model
