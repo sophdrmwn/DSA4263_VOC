cd18193ef5b0023cba8f659ab273e1555d55c74b
Manman Zhang <zhoujiuliang0521@gmail.com>
2023-04-03 19:38:21 +0800

    fix

--- src/sentiment_analysis/train/train_xgboost.py
+++ src/sentiment_analysis/train/train_xgboost.py
@@ -29,7 +29,7 @@ def train_xgboost(X_train, y_train):
         "max_depth": [3, 4, 5, 6, 8, 10, 12, 15],
         "min_child_weight": [1, 3, 5, 7],
         "gamma": [0.0, 0.1, 0.2, 0.3, 0.4],
-        "colsample_bytree": [0.3, 0, 4, 0.5, 0.7]
+        "colsample_bytree": [0.3, 0.4, 0.5, 0.7]
 
     }
     classifier = xgboost.XGBClassifier()
