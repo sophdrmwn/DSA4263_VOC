192e56998a5786a62871cfdd563c8ec767a7d899
Sophie Darmawan <sophiedarmawan@gmail.com>
2023-04-09 01:14:30 +0800

    added evaluation on best bert model

--- src/sentiment_analysis/train/train_bert.py
+++ src/sentiment_analysis/train/train_bert.py
@@ -1,11 +1,10 @@
-from transformations import *
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from transformers import BertForSequenceClassification, BertTokenizer, Trainer
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, roc_auc_score
+from transformers import pipeline, BertForSequenceClassification, BertTokenizer, Trainer, TrainingArguments
 import torch
 from ray import tune
 
 class Dataset(torch.utils.data.Dataset):
+
     def __init__(self, encodings, labels=None):
         self.encodings = encodings
         self.labels = labels
@@ -20,6 +19,7 @@ class Dataset(torch.utils.data.Dataset):
         return len(self.encodings["input_ids"])
 
 def compute_metrics(p):
+
     pred, labels = p
     pred = np.argmax(pred, axis=1)
 
@@ -28,41 +28,24 @@ def compute_metrics(p):
     return {"accuracy": accuracy}
 
 def ray_hp_space(trial):
+
     return {
-        'learning_rate': tune.choice([2e-5, 3e-5, 5e-5]),
-        'num_train_epochs': tune.choice([3, 5])
+        'learning_rate': tune.grid_search([3e-5, 5e-5]),
+        'num_train_epochs': tune.grid_search([2, 4])
     }
 
-def model_init(model_name = 'bert-based-uncased'):
-    return BertForSequenceClassification.from_pretrained(model_name)
+def model_init(trial):
 
-def preprocess_bert(df):
+    return BertForSequenceClassification.from_pretrained('bert-base-uncased')
 
-    df['clean_text'] = df['Text'].apply(lambda x: get_cleantext(x))
-    df['label'] = df.Sentiment.map({"positive": 1, "negative": 0})
+def train_bert(X_train, X_test, y_train, y_test):
 
-    df_clean = df[['clean_text', 'label']]
-    X = list(df_clean['clean_text'])
-    y = list(df_clean['label'])
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, stratify = y, random_state = 4263)
-
-    return X_train, X_test, y_train, y_test
-
-def initialise_bert(X_train, X_test, y_train, y_test, model_name = 'bert-base-uncased'):
-
-    tokenizer = BertTokenizer.from_pretrained(model_name)
-    model = BertForSequenceClassification.from_pretrained(model_name)
-
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     X_train_tokenized = tokenizer(X_train, padding = True, truncation = True, max_length = 512)
     X_test_tokenized = tokenizer(X_test, padding = True, truncation = True, max_length = 512)
 
     train_dataset = Dataset(X_train_tokenized, y_train)
     test_dataset = Dataset(X_test_tokenized, y_test)
-
-    return train_dataset, test_dataset
-
-def train_bert(train_dataset, test_dataset, n_trials = 2):
 
     trainer = Trainer(
         train_dataset = train_dataset,
@@ -75,7 +58,66 @@ def train_bert(train_dataset, test_dataset, n_trials = 2):
         direction = "maximize",
         backend = "ray",
         hp_space = ray_hp_space,
-        n_trials = n_trials
+        n_trials = 1
     )
 
     return best_trial
+
+def compute_eval_metrics(p):
+    pred, labels = p
+    pred = np.argmax(pred, axis=1)
+
+    accuracy = accuracy_score(y_true = labels, y_pred = pred)
+    recall = recall_score(y_true = labels, y_pred = pred)
+    precision = precision_score(y_true = labels, y_pred = pred)
+    f1 = f1_score(y_true = labels, y_pred = pred)
+    auc = roc_auc_score(y_true = labels, y_pred = pred)
+
+    return {"accuracy": accuracy, "recall": recall, "precision": precision, "f1": f1, "auc": auc}
+
+def eval_bert(X_train, X_test, y_train, y_test, best_params):
+    
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
+    
+    X_train_tokenized = tokenizer(X_train, padding = True, truncation = True, max_length = 512)
+    X_test_tokenized = tokenizer(X_test, padding = True, truncation = True, max_length = 512)
+
+    train_dataset = Dataset(X_train_tokenized, y_train)
+    test_dataset = Dataset(X_test_tokenized, y_test)
+    
+    training_args = TrainingArguments(
+        output_dir = 'results',
+        learning_rate = best_params['learning_rate'], 
+        num_train_epochs = best_params['num_train_epochs']
+    )
+    
+    trainer = Trainer(
+        model,
+        training_args,
+        train_dataset = train_dataset,
+        eval_dataset = test_dataset, 
+        compute_metrics = compute_eval_metrics
+    )
+    
+    trainer.train()
+    trainer.save_model('bert-tuned')
+    
+    trainer.evaluate()
+    
+def pred_bert(X_test):
+    
+    best_model = BertForSequenceClassification.from_pretrained('bert-tuned')
+    
+    sentiment_analysis = pipeline(
+        "sentiment-analysis", 
+        model = best_model, 
+        tokenizer = "bert-base-uncased"
+    )
+    
+    y_pred = []
+    for review in X_test:
+        result = sentiment_analysis(review)
+        y_pred.append(int(result[0]["label"][-1:]))
+    
+    return y_pred
