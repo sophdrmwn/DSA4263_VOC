f399167d5cf68f03044ff321396d9b47df064929
Manman Zhang <zhoujiuliang0521@gmail.com>
2023-04-09 04:35:06 +0800

    update xgboost train

--- src/sentiment_analysis/train/train_xgboost.py
+++ src/sentiment_analysis/train/train_xgboost.py
@@ -1,56 +1,69 @@
-from src.transformations import *
+from transformations import *
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, roc_auc_score
 import xgboost
 
-# loading raw data
-current_path = os.getcwd()
-df = pd.read_csv(current_path + '/data/reviews.csv', encoding='unicode_escape')
 
-# delete after including this part in pipeline
-# data cleaning
-df['clean_text'] = df['Text'].apply(lambda x: get_cleantext(x))
-df['Sentiment_num'] = df.Sentiment.map({"positive": 1, "negative": 0})
-X = df['clean_text'].to_list()
-y = df['Sentiment_num'].to_list()
-X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, stratify=y, random_state=4263
-    )
-
-def train_xgboost(X_train, y_train):
-    # compare the results of two feature engineering methods
-    X_train_tf = tf_idf(X_train)
-    X_train_word =word2vec(X_train)
-
+def train_xgboost(X_train_tf, X_train_word, y_train, metric= "accuracy"):
     # Hyperparameters for optimization
     params = {
-        "learning_rate": [0.001, 0.01, 0.1, 1],
-        "max_depth": [3, 4, 5, 6, 8, 10, 12, 15],
-        "min_child_weight": [1, 3, 5, 7],
-        "gamma": [0.0, 0.1, 0.2, 0.3, 0.4],
-        "colsample_bytree": [0.3, 0.4, 0.5, 0.7]
-
+        "learning_rate": [0.001, 0.01, 0.1],
+        "max_depth": [3, 5, 7],
+        "colsample_bytree": [0.5, 0.7, 0.9],
+        "subsample": [0.5, 0.7, 0.9],
+        "eval_metric": ["logloss"]
     }
+
     classifier = xgboost.XGBClassifier()
 
     # select best model
-    print("Start training xgboost model with feature engineering TF-IDF.\n")
-    grid_search_tf = GridSearchCV(classifier, param_grid=params, n_job=-1, scoring="accuracy", cv= 5,verbose=3)
-    grid_search_tf.fit(X_train_tf, y_train)
-    tf_best_estimator = grid_search_tf.best_estimator_
-    tf_best_score = grid_search_tf.best_score
+    grid_search = GridSearchCV(classifier, param_grid=params, n_jobs=-1, scoring= metric, cv=3,verbose=0)
+    grid_search.fit(X_train_tf, y_train)
+    # Print out the accuracy scores for each model trained
+    print("Accuracy scores for each model trained using TF-IDF in GridSearchCV:")
+    print(grid_search.cv_results_['mean_test_score'])
+    tf_best_estimator = grid_search.best_estimator_
+    tf_best_score = grid_search.best_score_
 
-    print("Start training xgboost model with feature engineering word2vec.\n")
-    grid_search_word = GridSearchCV(classifier, param_grid=params, n_job=-1, scoring="accuracy", cv= 5,verbose=3)
-    grid_search_word.fit(X_train_word, y_train)
-    word_best_estimator = grid_search_word.best_estimator_
-    word_best_score = grid_search_word.best_score
+    grid_search.fit(X_train_word, y_train)
+    # Print out the accuracy scores for each model trained
+    print("Accuracy scores for each model trained using word2vec in GridSearchCV:")
+    print(grid_search.cv_results_['mean_test_score'])
+    word_best_estimator = grid_search.best_estimator_
+    word_best_score = grid_search.best_score_
 
     # return best model
     if tf_best_score < word_best_score:
-        print("Successfully run the model with word2vec and return best estimator and score\n")
-        return word_best_estimator, word_best_score
+        print("Training on X_train_word gives the best estimator and score: " + str(word_best_score))
+        better_fe_method = "word"
+        return word_best_estimator, better_fe_method
     else:
-        print("Successfully run the model with word2vec and return best estimator and score\n")
-        return tf_best_estimator, tf_best_score
+        print("Training on X_train_tf gives the best estimator and score: " + str(tf_best_score))
+        better_fe_method = "tfidf"
+        return tf_best_estimator, better_fe_method
+    
+def eval_xgboost(best_estimator, better_fe_method, X_train_tf, X_train_word, X_test_word, X_test_word, y_test):
+    if fe_method =="word":
+        # Using the final xgboost tuning paramters, refit the model with the entire training set
+        best_estimator.fit(X_train_word)
+        y_pred = best_estimator(X_test_word)
+    else:
+        best_estimator.fit(X_train_tf)
+        y_pred = best_estimator(X_test_tf)
+        
+    # evalution metrics
+    acc = accuracy_score(y_test, y_pred)
+    pre = precision_score(y_test, y_pred)
+    recall = recall_score(y_test,y_pred)
+    f1 = f1_score(y_test, y_pred)  
+    auc = roc_auc_score(y_test, y_pred)
+    
+    return {"accuracy": accuracy, "recall": recall, "precision": precision, "f1": f1, "auc": auc}
+
+                       
+                       
+                       
+                       
+                       
