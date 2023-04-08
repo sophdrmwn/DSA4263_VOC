a58c48a93b9cc32714ddbeb0b2689364efee335b
Niceguy98 <52200957+Niceguy98@users.noreply.github.com>
2023-04-08 18:28:25 +0800

    edit vader and textblob

--- src/sentiment_analysis/train/train_textblob.py
+++ src/sentiment_analysis/train/train_textblob.py
@@ -1,12 +1,9 @@
 import pandas as pd
 from textblob import TextBlob
-from sklearn.metrics import accuracy_score
 
 def train_textblob(df):
     
-    df['TextBlob_sentiment'] = df['Text'].apply(lambda x: TextBlob(x).sentiment.polarity).apply(getTextblobSentiment)
-
-    return df
+    return df['Text'].apply(lambda x: TextBlob(x).sentiment.polarity).apply(getTextblobSentiment)
 
 
 def getTextblobSentiment(value):
--- src/sentiment_analysis/train/train_vader.py
+++ src/sentiment_analysis/train/train_vader.py
@@ -1,17 +1,14 @@
 import pandas as pd 
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import accuracy_score
 
 def train_vader(df):
     
     df['Vader_polarity'] = df['Text'].apply(getPolarity)
     scaler = MinMaxScaler()
     df[['Vader_polarity_scaled']] = scaler.fit_transform(df[['Vader_polarity']])
-    df['Vader_sentiment'] = df['Vader_polarity_scaled'].apply(getVaderSentiment)
+    return df['Vader_polarity_scaled'].apply(getVaderSentiment)
     
-    return df
-
 
 def getPolarity(sentence):
     sid_obj = SentimentIntensityAnalyzer()
