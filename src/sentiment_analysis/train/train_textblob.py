43809bf3442f7d66ae7995ce8820a6b64ca8af7d
Niceguy98 <52200957+Niceguy98@users.noreply.github.com>
2023-04-12 23:13:07 +0800

    added comments to vader and textblob py files

--- src/sentiment_analysis/train/train_textblob.py
+++ src/sentiment_analysis/train/train_textblob.py
@@ -9,9 +9,13 @@ def train_textblob(data):
     TextBlob_sentiment = []
 
     for item in data:
+        # offset polarity score (in float) from range [-1,1] to [0,1]
         offset = (TextBlob(item).sentiment.polarity + 1)/2
+        # convert offset float score in [0,1] to integer value in range [1,20]
         binned = np.digitize(20 * offset, np.array([1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])) + 1
+        # simulate probabilities of each class based on a normal distribution
         simulated_probs = scipy.stats.norm.pdf(np.array([1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]), binned, scale=0.5)
+        # add index of highest probability to list
         TextBlob_sentiment.append(simulated_probs.argmax())
 
     final =  [0 if x <= 10  else 1 for x in TextBlob_sentiment]
--- src/sentiment_analysis/train/train_vader.py
+++ src/sentiment_analysis/train/train_vader.py
@@ -8,9 +8,13 @@ def train_vader(data):
     Vader_sentiment = []
 
     for item in data:
+        # offset polarity score (in float) from range [-1,1] to [0,1]
         offset = (getPolarity(item) + 1)/2
+        # convert offset float score in [0,1] to integer value in range [1,20]
         binned = np.digitize(20 * offset, np.array([1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])) + 1
+        # simulate probabilities of each class based on a normal distribution
         simulated_probs = scipy.stats.norm.pdf(np.array([1, 2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]), binned, scale=0.5)
+        # add index of highest probability to list
         Vader_sentiment.append(simulated_probs.argmax())
 
     final =  [0 if x <= 10  else 1 for x in Vader_sentiment]
