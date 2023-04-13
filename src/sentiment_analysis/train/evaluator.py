43da81fe663fc0e1df54a8d4f2363909a6d23f4f
Sophie Darmawan <sophiedarmawan@gmail.com>
2023-04-14 00:50:55 +0800

    added plots for model comparison

--- src/sentiment_analysis/train/evaluator.py
+++ src/sentiment_analysis/train/evaluator.py
@@ -1,4 +1,5 @@
-from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, roc_auc_score
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, roc_auc_score, roc_curve
+import matplotlib.pyplot as plt
 
 def get_metrics(y_pred, y_test, results_dict, model_name, y_pred_score = None):
     # evalution metrics
@@ -11,11 +12,11 @@ def get_metrics(y_pred, y_test, results_dict, model_name, y_pred_score = None):
     if y_pred_score is not None:
         auc = roc_auc_score(y_test, y_pred_score)
     
-    metrics = {"accuracy": acc, 
-               "recall": recall, 
-               "precision": pre,
-               "f1": f1, 
-               "auc": auc}
+    metrics = {"Accuracy": acc, 
+               "Recall": recall, 
+               "Precision": pre,
+               "F1": f1, 
+               "AUC-ROC": auc}
     print(metrics)
 
     for metric in results_dict.keys():
@@ -39,3 +40,10 @@ def model_comparison(results_dict):
 
     print(best_model + ' performed the best among all the models.')
     print(rank_dict)
+
+def plot_roc_curve(y_test, y_pred):
+
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred)
+    plt.plot(fpr, tpr)
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
